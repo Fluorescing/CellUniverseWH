d919a161b8e71eec9f1301354d3c9a95038999b5
Shawna Stahlheber <Fluorescing@users.noreply.github.com>
2020-01-12 19:55:19 -0800

    update graysynth test and include input imgs

--- regression-tests/graysynthetic/test.sh
+++ regression-tests/graysynthetic/test.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-
 die() { echo "$@" >&2; exit 1; }
 
 echo "Testing simulated annealing with non-thresholded grayscale synthetic images"
 
-TEST_DIR=./regression-tests/simanneal
+TEST_DIR=./regression-tests/graysynthetic
 [ -d "$TEST_DIR" ] || die "Must run from the repository's root directory!"
 
-# create the test output dir if it doesn't exist
+# create the test output and debug dirs if they don't exist
 mkdir -p $TEST_DIR/output
 rm -f $TEST_DIR/output/*
+mkdir -p $TEST_DIR/debug
+rm -f $TEST_DIR/debug/*
 
 # TODO: change temp and endtemp to what works best for the new synth images
 if python3 "./main.py" \
     --start 0 \
     --finish 6 \
     --graysynthetic Ture\
-    --debug "./debug" \
-    --input "./input_jy/original_%03d.jpg" \
-    --output "./output" \
+    --debug "$TEST_DIR/debug" \
+    --input "$TEST_DIR/input/original_%03d.png" \
+    --output "$TEST_DIR/output" \
     --config "./config.json" \
     --initial "./cells.0.csv" \
     --temp 10 \
